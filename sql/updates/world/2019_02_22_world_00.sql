a78e59d7c2d4fe976f310166a4deaafb8ec13732
Steveo8679 <43922859+Steveo8679@users.noreply.github.com>
2019-02-22 17:04:14 +1000

    DB/SQL Add tele ShrineSevenStars

---
+++ sql/updates/world/2019_02_22_world_00.sql
@@ -0,0 +1,2 @@
+-- Teleport Shrine of Seven Stars
+INSERT INTO `world`.`game_tele` (`position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES ('872.68', '289.144', '503.226', '0.52296', '870', 'ShrineSevenStars');
